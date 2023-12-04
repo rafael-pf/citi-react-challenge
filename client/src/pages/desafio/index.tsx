851e0ef6e62f7181d3eb3b955291f444a0a1cdac
Rafael <rafapazfernandes@gmail.com>
2023-12-04 20:26:41 -0300

    tentei comecar mas ta dificil

--- client/src/pages/desafio/index.tsx
+++ client/src/pages/desafio/index.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+//como importa a imagem :(
+import { Button, TextField, Typography, Box, Container } from "@mui/material";
+
+
+//duas divs (esquerda e direta)
+//div da direita: apenas a imagem de fundo
+//div da esquerda: padding, logo, e box principal
 
 
 export default function Desafio() {
-    return (
-        <h1>Desafio!!</h1>
-    );
+  return (
+    <h1>eu si divirto</h1>
+  );
 }
